a753ae8820aef4a901c851960a255a7885f0b1e1
anne <mocchapi@gmail.com>
2023-06-05 13:49:25 +0200

    add pibby theme by mal

---
+++ sources/pibby/style.js
@@ -0,0 +1,356 @@
+{"main":
+ {"style": "background-repeat: no-repeat; font-family: 'Austie Bost Kitten Klub'; font-size: 14px;",
+  "background-image": "$path/ebg.png",
+  "size": [801, 555],
+  "icon": "$path/trayicon.png",
+  "newmsgicon": "$path/trayicon2.png",
+  "windowtitle": "MEOWMEOWMEOW",
+  "close": { "image": "$path/x.gif",
+             "loc": [315, 26]},
+  "minimize": { "image": "$path/m.gif",
+                "loc": [300, 32]},
+  "menubar": { "style": "font-family: 'Austie Bost Kitten Klub'; font-size: 14px; color:#4a4a4a" },
+  "menu" : { "style": "font-family: 'Austie Bost Kitten Klub'; font-size: 14px; background-color: #E693FF;border:2px solid #934CFF",
+             "menuitem": "margin-right:25px;",
+             "selected": "background-color: #934CFF",
+             "loc": [480,30]
+           },
+  "sounds": { "alertsound": "$path/alarm.wav",
+                       "memosound": "$path/alarm2.wav",
+                       "ceasesound": "$path/cease.wav" },
+  "menus": {"client": {"_name": "user :3",
+                       "options": "options",
+                       "memos": "meowmos :33c",
+                       "logviewer": "meowmories",
+                       "randen": "random pounce!! ;3",
+                       "userlist": "palsies",
+                       "addgroup": "litter :33",
+                       "talk": "pounce!",
+                       "import": "import",
+                       "reconnect": "reconnect",
+                       "idle": "eepy,,,",
+                       "exit": "leave :3c",
+                       "console": "Console"},
+            "profile": {"_name": "pr0file",
+                        "switch": "switch",
+                        "color": "c0lor :3c",
+                        "theme": "theme!!",
+                        "block": "meanies",
+                        "quirks": "quirks"},
+            "help": { "_name": "help?!?",
+                      "about": "abowt!",
+                      "rules": "r00ls!",
+                      "help": "h3lp!?!",
+                      "calsprite": "mr names",
+                      "chanserv": "mr channels",
+                      "nickserv": "mr nicks",
+                      "reportbug": "report bug :3"},
+            "rclickchumlist": {"pester": "pounce!",
+                               "removechum": "scratch >:3",
+                               "blockchum": "permascratch >:cc3c",
+                               "report": "repawrt.",
+                               "addchum": "snuggle!",
+                               "viewlog": "view meowmory!!",
+                               "notes": "notesies!!",
+                               "unblockchum": "unpermascratch",
+                               "removegroup": "remove litter,,,",
+                               "renamegroup": "rename litter!!",
+                               "movechum": "move 2",
+                               "banuser": "ban usr",
+                               "opuser": "make litterking!1",
+                               "quirksoff": "no quirkz :3c",
+                               "voiceuser": "give v0ice",
+                               "quirkkill": "scr4tch quirk :33",
+                               "invitechum": "invite bubby!",
+                               "memosetting": "meowmo settingz",
+                               "memonoquirk": "no quirkz :3c",
+                               "memohidden": "hidden,,!",
+                               "memoinvite": "invit3 only,,,",
+                               "memomute": "m00te",
+                               "beeponmessage": "b33p on msg!!",
+                               "flashonmessage": "flash on msg!!",
+                               "mutenotifications": "m00te notifzs,," 
+                              }
+           },
+  "chums": { "style": "font-size: 16px; background-image:url($path/chumbg.png); background-color: #C2C4EB; background-repeat: no-repeat; font-family: 'Austie Bost Kitten Klub'; border: 0px; selection-background-color: #FF76AF; color: black;",
+             "loc": [440, 211],
+             "size": [289, 275],
+              "text": "",
+             "userlistcolor": "black",
+             "loc": [440, 211],
+             "size": [289, 275],
+             "moods": {
+
+                 "chummy": { "icon": "$path/chummy.gif", "color": "black" },
+
+                 "rancorous": { "icon": "$path/rancorous.gif", "color": "red" },
+
+                 "offline": { "icon": "$path/offline.gif", "color": "#4a4a4a"},
+
+                 
+                 "pleasant": { "icon": "$path/pleasant.gif", "color": "black" },
+
+                 "distraught": { "icon": "$path/distraught.gif", "color": "black" },
+
+                 "pranky": { "icon": "$path/pranky.gif", "color": "black" },
+
+
+                 "smooth": { "icon": "$path/smooth.gif", "color": "black" },
+
+                 "mystified": { "icon": "$path/mystified.gif", "color": "black" },
+
+                 "amazed": { "icon": "$path/amazed.gif", "color": "black" },
+
+                 "insolent": { "icon": "$path/insolent.gif", "color": "black" },
+
+                 "bemused": { "icon": "$path/bemused.gif", "color": "black" },
+
+
+                 "ecstatic": { "icon": "$path/ecstatic.gif", "color": "red" },
+
+                 "relaxed": { "icon": "$path/relaxed.gif", "color": "red" },
+
+                 "discontent": { "icon": "$path/discontent.gif", "color": "red" },
+
+                 "devious": { "icon": "$path/devious.gif", "color": "red" },
+
+                 "sleek": { "icon": "$path/sleek.gif", "color": "red" },
+                 
+                 "detestful": { "icon": "$path/detestful.gif", "color": "red" },
+
+                 "mirthful": { "icon": "$path/mirthful.gif", "color": "red" },
+
+                 "manipulative": { "icon": "$path/manipulative.gif", "color": "red" },
+
+                 "vigorous": { "icon": "$path/vigorous.gif", "color": "red" },
+
+                 "perky": { "icon": "$path/perky.gif", "color": "red" },
+
+                 "acceptant": { "icon": "$path/acceptant.gif", "color": "red" },
+
+                 "protective": { "icon": "$path/protective.png", "color": "#13649A" },
+
+                 "blocked": { "icon": "$path/blocked.gif", "color": "red" }
+
+             }
+           },
+  "trollslum": { 
+      "style": "background: #E693FF; border:2px solid #934CFF; font-family: 'Austie Bost Kitten Klub'",
+      "size": [195, 200],
+      "label": { "text": "MEANIES",
+                 "style": "color: rgba(0, 0, 0, 100%) ;font:bold; font-family: 'Austie Bost Kitten Klub';border:0px;" },
+      "chumroll": {"style": "border:2px solid #934CFF; background-color: black;color: #E48CFF;font: bold;font-family: 'Austie Bost Kitten Klubl';selection-background-color:#808080; " }
+  },
+  "mychumhandle": { "label": { "text": "",
+                               "loc": [0,0],
+                               "style": "color: rgba(255, 119, 164, 0%) ;font:bold; font-family: 'Austie Bost Kitten Klub';" },
+                    "handle": { "style": "background: rgba(255, 119, 164, 0%); color:#E48CFF; font-family:'Austie Bost Kitten Klub'; font-size:14px; text-align:left;",
+                                "loc": [0,0],
+                                "size": [0, 0] },
+                    "colorswatch": { "loc": [0,0],
+                                     "size": [0,0],
+                                     "text": "" },
+                    "currentMood": [1500, 1500]
+                  },
+  "defaultwindow": { "style": "background: #E693FF; font-family:'Austie Bost Kitten Klub';font:bold;selection-background-color:#919191; " 
+                   },
+  "addchum":  { "style": "background: rgba(255, 119, 164, 0%); border:0px; color: rgba(0, 0, 0, 0%);",
+              "loc": [443,144],
+              "size": [284, 60],
+                "text": ""
+              },
+  "pester": { "style": "background:  rgba(255, 119, 164, 0%); border:0px; font: bold; color:  rgba(255, 119, 164, 0%); font-family:'Austie Bost Kitten Klub';",
+              "pressed" : "background-image:url($path/pesterhold.png);",
+                "loc": [0,0],
+                "size": [0, 0],
+              "text": ""
+            },
+  "block": { "style": "background:  rgba(255, 119, 164, 0%); border:2px solid #DD63FF; font: bold; color:  rgba(255, 119, 164, 0%); font-family:'Austie Bost Kitten Klub';",
+             "loc": [0,0],
+             "size": [0, 0],
+             "text": ""
+           },
+  "defaultmood": 0,
+  "moodlabel": { "style": "",
+                 "loc": [20, 430],
+                 "text": ""
+               },
+  "moods": [
+      { "style": "background-image:url($path/mood1.png); border:0px;", 
+        "selected": "background-image:url($path/mood1c.png); border:0px;",
+        "loc": [0, 258],
+        "size": [100,110],
+         "text": "",
+        "icon": "",
+        "mood": 0
+      },
+      { "style": "background-image:url($path/mood2.png); border:0px;", 
+        "selected": "background-image:url($path/mood2c.png); border:0px;",
+        "loc": [106, 258],
+        "size": [100, 110],
+         "text": "",
+        "icon": "",
+        "mood": 19
+      },
+      { "style": "background-image:url($path/mood3.png); border:0px;", 
+        "selected": "background-image:url($path/mood3c.png); border:0px;",
+        "loc": [212, 258],
+        "size": [100, 110],
+         "text": "",
+        "icon": "",
+        "mood": 22
+      },
+      { "style": "background-image:url($path/mood4.png); border:0px;", 
+        "selected": "background-image:url($path/mood4c.png); border:0px;",
+        "loc": [318, 258],
+        "size": [100, 110],
+         "text": "",
+        "icon": "",
+        "mood": 4
+      },
+      { "style": "background-image:url($path/mood5.png); border:0px;", 
+        "selected": "background-image:url($path/mood5c.png); border:0px;",
+        "loc": [0, 382],
+        "size": [100, 110],
+         "text": "",
+        "icon": "",
+        "mood": 3
+      },
+      { "style": "background-image:url($path/mood6.png); border:0px;", 
+        "selected": "background-image:url($path/mood6c.png); border:0px;",
+        "loc": [106, 382],
+        "size": [100, 110],
+         "text": "",
+        "icon": "",
+        "mood": 20
+      },
+      { "style": "background-image:url($path/mood7.png); border:0px;", 
+        "selected": "background-image:url($path/mood7c.png); border:0px;",
+        "loc": [212, 382],
+        "size": [100, 110],
+         "text": "",
+        "icon": "",
+        "mood": 5
+      },
+      { "style": "background-image:url($path/mood8.png); border:0px;", 
+        "selected": "background-image:url($path/mood8c.png); border:0px;",
+        "loc": [318, 382],
+        "size": [100, 110],
+         "text": "",
+        "icon": "",
+        "mood": 1
+      },
+      { "style": "border:0px;", 
+        "selected": "border:0px;",
+        "loc": [0, 0],
+        "size": [100, 100],
+         "text": "",
+        "icon": "",
+        "mood": 2
+      }
+  ]
+ },
+ "convo":
+ {"style": "background-color: rgba(0, 0, 0, 0%);border-width: 5px; border-image:url($path/convobg.png) 5px; font-family: 'Austie Bost Kitten Klub'",
+  "tabstyle": "background-color: #E693FF; font-family: 'Austie Bost Kitten Klub'",
+  "scrollbar": { "style" : "padding-top:17px; padding-bottom:17px;width: 18px; background:  rgba(255, 119, 164, 0%); border:0px;",
+                 "handle": "border-width: 5px; border-image:url($path/scrollbg.png) 5px; min-height:60px;",
+                 "downarrow": "height:17px;border:0px solid #DD63FF;", 
+                 "darrowstyle": "image:url($path/downarrow.png);",
+                 "uparrow": "height:17px;border:0px solid #DD63FF;",
+                 "uarrowstyle": "image:url($path/uparrow.png);"
+               },
+  "margins": {"top": 35, "bottom": 35, "left": 32, "right": 20 },
+  "size": [480, 348],
+  "chumlabel": { "style": " background:  rgba(255, 119, 164, 0%); color: #E48CFF; border:0px; ",
+                 "align": { "h": "center", "v": "center" },
+                 "minheight": 0,
+                 "maxheight": 0,
+                 "text" : ""
+               },
+  "textarea": {
+      "style": "background-color: #E48CFF;  background-image: url(); font: bold; font-size: 14px; font-family: 'Austie Bost Kitten Klub'; border:2px solid #934CFF;text-align:center;"
+  },
+  "input": { "style": "background: #E48CFF; border:2px solid #934CFF;margin-top:10px; font: bold; font-size: 14px; font-family: 'Austie Bost Kitten Klub'" },
+  "tabs": {
+      "style": "",
+      "selectedstyle": "",
+      "newmsgcolor": "#E693FF",
+      "tabstyle": 0
+  },
+  "tabwindow" : {
+      "style": "background: #E693FF; font-family: 'Austie Bost Kitten Klub'"
+  },
+  "text": {
+      "beganpester": "pounced on",
+      "ceasepester": "pounced off",
+      "blocked": "permascratched",
+      "unblocked": "unpermascratched",
+      "blockedmsg": "did not listen from",
+      "openmemo": "l00ked @",
+      "joinmemo": "answerd the meowmo!!!",
+      "closememo": "went away from the meowmo,,,",
+      "kickedmemo": "you have been scratched from this memo >:cc3c!!",
+      "idle": "is now eepy,, zzzz,, zzz"
+  },
+  "systemMsgColor": "#808080"
+ },
+ "memos":
+ {"style": "background-color: rgba(0,0,0,0); border-width: 5px; border-image:url($path/convobg.png) 5px; font-family: 'Austie Bost Kitten Klub'; selection-background-color:#919191; ",
+  "size": [500,325],
+  "tabs": {
+      "style": "",
+      "selectedstyle": "",
+      "newmsgcolor": "#E693FF",
+      "tabstyle": 0
+  },
+  "scrollbar": { "style" : "padding-top:17px; padding-bottom:17px;width: 18px; background:  rgba(255, 119, 164, 0%); border:0px;",
+                 "handle": "border-width: 5px; border-image:url($path/scrollbg.png) 5px; min-height:60px;",
+                 "downarrow": "height:17px;border:0px;", 
+                 "darrowstyle": "image:url();",
+                 "uparrow": "height:17px;border:0px;",
+                 "uarrowstyle": "image:url();"
+               },
+  "label": { "text": "Bulletin Board: $channel",
+             "style": "margin-bottom: 7px; margin-top: 10px; background-color: rgba(0,0,0,0) ; background-image:url($path/nothing.png); color: #E48CFF; border:0px; font-size: 16px;",
+             "align": { "h": "center", "v": "center" },
+             "minheight": 47,
+             "maxheight": 47
+           },
+  "input": { "style": "background: #E48CFF; border:2px solid #DD63FF;margin-top:5px; margin-right:10px; margin-left:10px; font: bold; font-size: 14px; font-family: 'Austie Bost Kitten Klub'" },
+  "textarea": { "style": " background-color: #E48CFF; background-image:url();  font: bold; font-size: 14px; font-family: 'Austie Bost Kitten Klub'; border:2px solid #DD63FF;text-align:center; margin-right:10px; margin-left:10px;" },
+  "margins": {"top": 0, "bottom": 6, "left": 0, "right": 0 },
+  "userlist": { "width": 150,
+                "style": "border:2px solid #DD63FF; background: #E48CFF; font-family: 'Austie Bost Kitten Klub';selection-background-color:#808080; font-size: 14px;  margin-left:0px; margin-right:10px;"
+              },
+  "time": { "text": { "width": 75, 
+                      "style": " border: 2px solid #934CFF; background: #E48CFF; font-size: 12px; margin-top: 5px; margin-right: 5px; margin-left: 5px; font-family:'Austie Bost Kitten Klub';font:bold;" 
+                    },
+            "slider": { "style": "border: 0px;",
+                        "groove": "",
+                        "handle": ""
+                      },
+            "buttons": { "style": "color: black; font: bold; border: 2px solid #DD63FF; font-size: 12px; background: green; margin-top: 5px; margin-right: 5px; margin-left: 5px; padding: 2px; width: 50px;" }, 
+            "arrows": { "left": "$path/leftarrow.png", 
+                        "right": "$path/rightarrow.png",
+                        "style": " border:0px; margin-top: 5px; margin-right:10px;" 
+                      }
+          },
+  "systemMsgColor": "#808080",
+  "op": { "icon": "$path/sleek.gif" }
+ },
+
+ "toasts":
+ {
+   "width": 210,
+   "height": 100,
+   "style": "background: white;",
+   "icon": { "signin": "$path/../pibby/pranky.gif",
+             "signout": "$path/../pibby/trayicon2.gif",
+             "style": "border: 2px solid #934CFF; border-width: 2px 0px 0px 2px;" },
+   "title": { "minimumheight": 50,
+              "style": "border: 2px solid #934CFF; border-width: 2px 2px 0px 0px; padding: 5px; font-weight:bold;"
+            },
+   "content": { "style": "background: #934CFF; color: #FFAAC6; padding: 5px;" }
+ }
+}
