0ea1426e5ee5a269a150592b488e866ef95ae6cd
anne <mocchapi@gmail.com>
2023-07-02 21:20:28 +0200

    JEERUBIUM & CHEERUBIUM + their variants by mal added

---
+++ sources/CHEERUBIUM/style.js
@@ -0,0 +1,479 @@
+{
+	"main": {
+		"style": "background-color: #ff000000; background-repeat: no-repeat; ",
+		"background-image": "$path/fbg.png",
+		"size": [
+			650,
+			450
+		],
+		"icon": "$path/trayicon.png",
+		"newmsgicon": "$path/trayicon2.png",
+		"windowtitle": "CHEERUBIUM",
+		"close": {
+			"image": "$path/x.png",
+			"loc": [
+				4,
+				4
+			]
+		},
+		"minimize": {
+			"image": "$path/m.png",
+			"loc": [
+				15,
+				7
+			]
+		},
+		"menubar": {
+			"style": "font-family: 'Arial'; font-size: 11px; color: #ff000000; "
+		},
+		"menu": {
+			"style": "font-family: 'Arial'; font-size: 11px; color: #ff000000; background-color: #ffc2c2c2; border: 1px solid #545454; ",
+			"selected": "background-color: #ff545454; ",
+			"menuitem": "margin-right: 14px; ",
+			"loc": [
+				350,
+				70
+			]
+		},
+		"sounds": {
+			"alertsound": "$path/alarm.wav",
+			"memosound": "$path/alarm2.wav"
+		},
+		"menus": {
+			"client": {
+				"_name": "cheerUbiUm",
+				"options": "options",
+				"memos": "memos",
+				"logviewer": "cheerlogs",
+				"randen": "random encoUnter!",
+				"userlist": "all Users",
+				"addgroup": "add groUp",
+				"talk": "cheer someone Up!",
+				"import": "import U2;",
+				"reconnect": "reconnect",
+				"idle": "idle",
+				"exit": "go to sleep...",
+				"console": "console"
+			},
+			"profile": {
+				"_name": "view",
+				"switch": "change disgUise",
+				"theme": "theme",
+				"color": "hemospectrUm",
+				"block": "blocked...",
+				"quirks": "qUirks!"
+			},
+			"help": {
+				"_name": "help!",
+				"about": "aboUt",
+				"help": "oUtdated",
+				"calsprite": "calsprite?",
+				"chanserv": "chanserv?",
+				"nickserv": "nickserv?",
+				"rules": "rUles!!!",
+				"reportbug": "report bUg"
+			},
+			"rclickchumlist": {
+				"pester": "cheer!",
+				"removechum": "dispose",
+				"report": "tattle",
+				"blockchum": "block",
+				"addchum": "add friend!",
+				"viewlog": "view cheerlog",
+				"notes": "edit notes...",
+				"unblockchum": "Unblock",
+				"removegroup": "remove groUp",
+				"renamegroup": "rename groUp",
+				"movechum": "move to",
+				"banuser": "banish!",
+				"opuser": "promote",
+				"voiceuser": "give voice",
+				"quirkkill": "cUt qUirk",
+				"quirksoff": "disable own qUirks",
+				"invitechum": "invite friend!",
+				"memosetting": "memo settings",
+				"memonoquirk": "disable all qUirks!",
+				"memohidden": "hidden",
+				"memoinvite": "invite only",
+				"memomute": "silence",
+				"beeponmessage": "beep on message",
+				"flashonmessage": "flash on message",
+				"mutenotifications": "mUte notifications"
+			}
+		},
+		"chums": {
+			"style": "font-size: 12px; background: white; border: 0px; font-family: 'Arial'; selection-background-color: #ff000000; ",
+			"scrollbar": {
+				"style": "background-color: #ffc2c2c2; ",
+				"handle": "background-color:#e02413;height:20px;border:2px solid #ff2612;",
+				"downarrow": "",
+				"darrowstyle": "",
+				"uparrow": "",
+				"uarrowstyle": ""
+			},
+			"loc": [
+				2,
+				90
+			],
+			"size": [
+				171,
+				357
+			],
+			"userlistcolor": "#ff000000",
+			"moods": {
+				"chummy": {
+					"icon": "$path/chummy.png",
+					"color": "#ff63ea00"
+				},
+				"rancorous": {
+					"icon": "$path/rancorous.png",
+					"color": "#ffff0000"
+				},
+				"offline": {
+					"icon": "$path/offline.png",
+					"color": "#ff000000"
+				},
+				"pleasant": {
+					"icon": "$path/pleasant.png",
+					"color": "#ffd69df8"
+				},
+				"distraught": {
+					"icon": "$path/distraught.png",
+					"color": "#ff706eba"
+				},
+				"pranky": {
+					"icon": "$path/pranky.png",
+					"color": "#ff0000ff"
+				},
+				"smooth": {
+					"icon": "$path/smooth.png",
+					"color": "#ffff0000"
+				},
+				"ecstatic": {
+					"icon": "$path/ecstatic.png",
+					"color": "#ff99004d"
+				},
+				"relaxed": {
+					"icon": "$path/relaxed.png",
+					"color": "#ff078446"
+				},
+				"discontent": {
+					"icon": "$path/discontent.png",
+					"color": "#ffa75403"
+				},
+				"devious": {
+					"icon": "$path/devious.png",
+					"color": "#ff008282"
+				},
+				"sleek": {
+					"icon": "$path/sleek.png",
+					"color": "#ffa1a100"
+				},
+				"detestful": {
+					"icon": "$path/detestful.png",
+					"color": "#ff6a006a"
+				},
+				"mirthful": {
+					"icon": "$path/mirthful.png",
+					"color": "#ff450077"
+				},
+				"manipulative": {
+					"icon": "$path/manipulative.png",
+					"color": "#ff004182"
+				},
+				"vigorous": {
+					"icon": "$path/vigorous.png",
+					"color": "#ff0021cb"
+				},
+				"perky": {
+					"icon": "$path/perky.png",
+					"color": "#ff406600"
+				},
+				"acceptant": {
+					"icon": "$path/acceptant.png",
+					"color": "#ffa10000"
+				},
+				"protective": {
+					"icon": "$path/protective.png",
+					"color": "#ff15ff00"
+				},
+				"blocked": {
+					"icon": "$path/blocked.png",
+					"color": "#ff000000"
+				}
+			}
+		},
+		"trollslum": {
+			"style": "background: rgb(0, 118, 0); font-family: 'Arial'; ",
+			"size": [
+				175,
+				461
+			],
+			"label": {
+				"text": "blocked...",
+				"style": "color: #ff000000; font: bold; font-family: 'Arial'; border: 0px; "
+			},
+			"chumroll": {
+				"style": "border: 0px; background-color: #ffffffff; padding: 5px; font-family: 'Arial'; selection-background-color: #ff000000; "
+			}
+		},
+		"mychumhandle": {
+			"currentMood": [
+				700,
+				0
+			],
+			"label": {
+				"text": "",
+				"loc": [
+					0,
+					0
+				],
+				"style": "color: #ff000000; "
+			},
+			"handle": {
+				"style": "background: rgba(0,0,0,0); color: #ff000000; ",
+				"loc": [
+					0,
+					0
+				],
+				"size": [
+					0,
+					0
+				]
+			},
+			"colorswatch": {
+				"loc": [
+					0,
+					0
+				],
+				"size": [
+					0,
+					0
+				],
+				"text": ""
+			}
+		},
+		"defaultwindow": {
+			"style": "background: rgb(0, 118, 0); font-family: 'Arial'; font: bold; selection-background-color: #ff545454; "
+		},
+		"addchum": {
+			"style": "background: rgba(0,0,0,0); border: 0px; color: #ff000000; ",
+			"loc": [
+				475,
+				67
+			],
+			"size": [
+				0,
+				0
+			],
+			"text": ""
+		},
+		"pester": {
+			"style": "background: rgba(0,0,0,0); border: 0px; color: #ff000000; ",
+			"loc": [
+				0,
+				0
+			],
+			"size": [
+				0,
+				0
+			],
+			"text": ""
+		},
+		"block": {
+			"style": "background: rgba(0,0,0,0); border: 0px; color: #ff000000; ",
+			"loc": [
+				1500,
+				202
+			],
+			"size": [
+				71,
+				22
+			],
+			"text": ""
+		},
+		"defaultmood": 18,
+		"moodlabel": {
+			"style": "",
+			"loc": [
+				0,
+				0
+			],
+			"text": ""
+		},
+		"moods": [
+			{
+				"style": "border: 0px; ",
+				"selected": "background-image: url($path/lime.png); border: 0px; ",
+				"loc": [
+					418,
+					141
+				],
+				"size": [
+					217,
+					270
+				],
+				"text": "",
+				"icon": "$path/",
+				"mood": 18
+			},
+			{
+				"style": "border: 0px; color: #ff000000; ",
+				"selected": "border: 0px; color: #ff000000; ",
+				"loc": [
+					12,
+					117
+				],
+				"size": [
+					435,
+					18
+				],
+				"text": "",
+				"icon": "$path/",
+				"mood": 2
+			}
+		]
+	},
+	"convo": {
+		"style": "background: rgb(190, 19, 4); font-family: 'Arial'; ",
+		"scrollbar": {
+			"style": "",
+			"handle": ""
+		},
+		"margins": {
+			"top": 22,
+			"bottom": 9,
+			"left": 10,
+			"right": 4
+		},
+		"size": [
+			400,
+			250
+		],
+		"chumlabel": {
+			"style": "background-image: url($path/chumlabelbg.png); background-color: #ff000000; background-repeat: no-repeat; color: #ffffffff; padding: 2px; border: 1px solid #c2c2c2; margin-bottom: 4px; ",
+			"align": {
+				"h": "left",
+				"v": "center"
+			},
+			"minheight": 22,
+			"maxheight": 36,
+			"text": "cheering: $handle!"
+		},
+		"textarea": {
+			"style": "background: rgb(200, 200, 200); border: 2px solid #c2c2c2; font-size: 14px; "
+		},
+		"input": {
+			"style": "background: rgb(200, 200, 200); margin-top: 5px; border: 1px solid #c2c2c2; margin-right: 54px; font-size: 12px; "
+		},
+		"tabwindow": {
+			"style": "background: rgb(0, 118, 0); font-family: 'Arial'; "
+		},
+		"tabs": {
+			"style": "",
+			"selectedstyle": "",
+			"newmsgcolor": "#ff00ff11",
+			"tabstyle": 0
+		},
+		"text": {
+			"beganpester": "began cheering",
+			"ceasepester": "gave up cheering",
+			"blocked": "blocked",
+			"unblocked": "forgave",
+			"openmemo": "opened memo on board",
+			"joinmemo": "responded to memo",
+			"closememo": "ceased responding to memo",
+			"kickedmemo": "you're not allowed to speak..."
+		},
+		"systemMsgColor": "#ff646464"
+	},
+	"memos": {
+		"memoicon": "$path/memo.png",
+		"style": "background: rgb(0, 118, 0); font-family: 'Arial'; ",
+		"size": [
+			450,
+			300
+		],
+		"tabs": {
+			"style": "",
+			"selectedstyle": "",
+			"newmsgcolor": "#ff00ff1c",
+			"tabstyle": 0
+		},
+		"label": {
+			"text": "bulletin board?: $channel",
+			"style": "background-image: url($path/chumlabelbg.png); background-color: #ff000000; background-repeat: no-repeat; color: #ffffffff; padding: 2px; border: 1px solid #c2c2c2; margin-bottom: 4px; ",
+			"align": {
+				"h": "left",
+				"v": "center"
+			},
+			"minheight": 18,
+			"maxheight": 36
+		},
+		"textarea": {
+			"style": "background: rgb(200, 200, 200); border: 2px solid #c2c2c2; font-size: 12px; "
+		},
+		"input": {
+			"style": "background: rgb(200, 200, 200); margin-top: 5px; border: 1px solid #c2c2c2; font-size: 12px; margin-bottom: 5px; "
+		},
+		"margins": {
+			"top": 22,
+			"bottom": 10,
+			"left": 9,
+			"right": 4
+		},
+		"userlist": {
+			"width": 125,
+			"style": "font-size: 12px; background: rgb(200, 200, 200); margin-bottom: 5px; border: 2px solid #c2c2c2; padding: 5px; font-family: 'Arial'; selection-background-color: #ff000000; "
+		},
+		"time": {
+			"text": {
+				"width": 75,
+				"style": "color: #ff000000; font: bold; border: 1px solid #c2c2c2; background: rgb(200, 200, 200); height: 19px; "
+			},
+			"slider": {
+				"style": "border: 1px solid #00c2c2c2; ",
+				"groove": "border-image: url($path/timeslider.png); ",
+				"handle": "image: url($path/chummy.png); "
+			},
+			"buttons": {
+				"style": "border: 1px solid #00ffffff; height: 17px; width: 50px; color: #ff007600; font-family: 'Arial'; background: rgb(190, 19, 4); margin-left: 2px; "
+			},
+			"arrows": {
+				"left": "$path/leftarrow.png",
+				"right": "$path/rightarrow.png",
+				"style": "width: 19px; height: 19px; border: 0px; margin-left: 2px; "
+			}
+		},
+		"systemMsgColor": "#ff646464",
+		"op": {
+			"icon": "$path/op.png"
+		},
+		"halfop": {
+			"icon": "$path/halfop.png"
+		},
+		"voice": {
+			"icon": "$path/voice.png"
+		},
+		"founder": {
+			"icon": "$path/founder.png"
+		}
+	},
+	"toasts": {
+		"width": 210,
+		"height": 100,
+		"style": "background: rgb(0, 118, 0); ",
+		"icon": {
+			"signin": "$path/trayicon.png",
+			"signout": "$path/trayicon2.png",
+			"style": "border: 0px; "
+		},
+		"title": {
+			"minimumheight": 50,
+			"style": "border: 0px; padding: 5px; font-weight: bold; color: #ffffffff; "
+		},
+		"content": {
+			"style": "background: rgb(177, 177, 177); color: #ff000000; padding: 5px; "
+		}
+	}
+}
---
+++ sources/JEERUBIUM/style.js
@@ -0,0 +1,479 @@
+{
+	"main": {
+		"style": "background-color: #ff000000; background-repeat: no-repeat; ",
+		"background-image": "$path/fbg.png",
+		"size": [
+			650,
+			450
+		],
+		"icon": "$path/trayicon.png",
+		"newmsgicon": "$path/trayicon2.png",
+		"windowtitle": "JEERUBIUM",
+		"close": {
+			"image": "$path/x.png",
+			"loc": [
+				4,
+				4
+			]
+		},
+		"minimize": {
+			"image": "$path/m.png",
+			"loc": [
+				15,
+				7
+			]
+		},
+		"menubar": {
+			"style": "font-family: 'Arial'; font-size: 11px; color: #ff000000; "
+		},
+		"menu": {
+			"style": "font-family: 'Arial'; font-size: 11px; color: #ff000000; background-color: #ffc2c2c2; border: 1px solid #545454; ",
+			"selected": "background-color: #ff545454; ",
+			"menuitem": "margin-right: 14px; ",
+			"loc": [
+				330,
+				70
+			]
+		},
+		"sounds": {
+			"alertsound": "$path/alarm.wav",
+			"memosound": "$path/alarm2.wav"
+		},
+		"menus": {
+			"client": {
+				"_name": "JEERuBIuM.",
+				"options": "OPTIONS.",
+				"memos": "MEMOS.",
+				"logviewer": "JEERLOGS.",
+				"randen": "RANDOM IDIOT.",
+				"userlist": "ALL IDIOTS.",
+				"addgroup": "ADD GROuP.",
+				"talk": "OWN SOME IDIOT.",
+				"import": "import U2;",
+				"reconnect": "RECONNECT.",
+				"idle": "'IDLE'.",
+				"exit": "BETTER THINGS TO DO.",
+				"console": "console"
+			},
+			"profile": {
+				"_name": "VIEW.",
+				"switch": "PLAY PRETEND.",
+				"theme": "THEME.",
+				"color": "STUPID DUMB OPTION.",
+				"block": "STUPID DUMB PEOPLE.",
+				"quirks": "ANOTHER STUPID DUMB OPTION."
+			},
+			"help": {
+				"_name": "\"ASSISTANCE\".",
+				"about": "ABOuT.",
+				"help": "OuTDATED.",
+				"calsprite": "CALSPRITE??",
+				"chanserv": "CHANSERV???",
+				"nickserv": "NICKSERV????",
+				"rules": "'RULES'. (IRRELEVANT)",
+				"reportbug": "REPORT BuG."
+			},
+			"rclickchumlist": {
+				"pester": "JEER.",
+				"removechum": "ELIMINATE.",
+				"report": "TATTLE.",
+				"blockchum": "ANNIHILATE.",
+				"addchum": "TRACK MORON.",
+				"viewlog": "VIEW JEERLOG.",
+				"notes": "EDIT NOTES.",
+				"unblockchum": "uNANNIHILATE.",
+				"removegroup": "REMOVE GROuP.",
+				"renamegroup": "RENAME GROuP.",
+				"movechum": "MOVE TO",
+				"banuser": "PuSH OFF A HILL.",
+				"opuser": "PROMOTE.",
+				"voiceuser": "GIVE VOICE.",
+				"quirkkill": "KILL 'QuIRK'.",
+				"quirksoff": "uNQuIRK YOuRSELF.",
+				"invitechum": "INVITE IDIOT.",
+				"memosetting": "MEMO SETTINGS.",
+				"memonoquirk": "KILL ALL 'QuIRKS'.",
+				"memohidden": "HIDDEN.",
+				"memoinvite": "INVITE ONLY.",
+				"memomute": "SHuT uP.",
+				"beeponmessage": "BEEP ON MESSAGE.",
+				"flashonmessage": "FLASH ON MESSAGE.",
+				"mutenotifications": "SHuT uuuP«!"
+			}
+		},
+		"chums": {
+			"style": "font-size: 12px; background: white; border: 0px; font-family: 'Arial'; selection-background-color: #ff000000; ",
+			"scrollbar": {
+				"style": "background-color: #ffc2c2c2; ",
+				"handle": "background-color:#e02413;height:20px;border:2px solid #ff2612;",
+				"downarrow": "",
+				"darrowstyle": "",
+				"uparrow": "",
+				"uarrowstyle": ""
+			},
+			"loc": [
+				2,
+				90
+			],
+			"size": [
+				171,
+				357
+			],
+			"userlistcolor": "#ff000000",
+			"moods": {
+				"chummy": {
+					"icon": "$path/chummy.png",
+					"color": "#ff63ea00"
+				},
+				"rancorous": {
+					"icon": "$path/rancorous.png",
+					"color": "#ffff0000"
+				},
+				"offline": {
+					"icon": "$path/offline.png",
+					"color": "#ff000000"
+				},
+				"pleasant": {
+					"icon": "$path/pleasant.png",
+					"color": "#ffd69df8"
+				},
+				"distraught": {
+					"icon": "$path/distraught.png",
+					"color": "#ff706eba"
+				},
+				"pranky": {
+					"icon": "$path/pranky.png",
+					"color": "#ff0000ff"
+				},
+				"smooth": {
+					"icon": "$path/smooth.png",
+					"color": "#ffff0000"
+				},
+				"ecstatic": {
+					"icon": "$path/ecstatic.png",
+					"color": "#ff99004d"
+				},
+				"relaxed": {
+					"icon": "$path/relaxed.png",
+					"color": "#ff078446"
+				},
+				"discontent": {
+					"icon": "$path/discontent.png",
+					"color": "#ffa75403"
+				},
+				"devious": {
+					"icon": "$path/devious.png",
+					"color": "#ff008282"
+				},
+				"sleek": {
+					"icon": "$path/sleek.png",
+					"color": "#ffa1a100"
+				},
+				"detestful": {
+					"icon": "$path/detestful.png",
+					"color": "#ff6a006a"
+				},
+				"mirthful": {
+					"icon": "$path/mirthful.png",
+					"color": "#ff450077"
+				},
+				"manipulative": {
+					"icon": "$path/manipulative.png",
+					"color": "#ff004182"
+				},
+				"vigorous": {
+					"icon": "$path/vigorous.png",
+					"color": "#ff0021cb"
+				},
+				"perky": {
+					"icon": "$path/perky.png",
+					"color": "#ff406600"
+				},
+				"acceptant": {
+					"icon": "$path/acceptant.png",
+					"color": "#ffa10000"
+				},
+				"protective": {
+					"icon": "$path/protective.png",
+					"color": "#ff15ff00"
+				},
+				"blocked": {
+					"icon": "$path/blocked.png",
+					"color": "#ff000000"
+				}
+			}
+		},
+		"trollslum": {
+			"style": "background: rgb(0, 118, 0); font-family: 'Arial'; ",
+			"size": [
+				175,
+				461
+			],
+			"label": {
+				"text": "BLOCKED.",
+				"style": "color: #ff000000; font: bold; font-family: 'Arial'; border: 0px; "
+			},
+			"chumroll": {
+				"style": "border: 0px; background-color: #ffffffff; padding: 5px; font-family: 'Arial'; selection-background-color: #ff000000; "
+			}
+		},
+		"mychumhandle": {
+			"currentMood": [
+				700,
+				0
+			],
+			"label": {
+				"text": "",
+				"loc": [
+					0,
+					0
+				],
+				"style": "color: #ff000000; "
+			},
+			"handle": {
+				"style": "background: rgba(0,0,0,0); color: #ff000000; ",
+				"loc": [
+					0,
+					0
+				],
+				"size": [
+					0,
+					0
+				]
+			},
+			"colorswatch": {
+				"loc": [
+					0,
+					0
+				],
+				"size": [
+					0,
+					0
+				],
+				"text": ""
+			}
+		},
+		"defaultwindow": {
+			"style": "background: rgb(0, 118, 0); font-family: 'Arial'; font: bold; selection-background-color: #ff545454; "
+		},
+		"addchum": {
+			"style": "background: rgba(0,0,0,0); border: 0px; color: #ff000000; ",
+			"loc": [
+				475,
+				67
+			],
+			"size": [
+				0,
+				0
+			],
+			"text": ""
+		},
+		"pester": {
+			"style": "background: rgba(0,0,0,0); border: 0px; color: #ff000000; ",
+			"loc": [
+				0,
+				0
+			],
+			"size": [
+				0,
+				0
+			],
+			"text": ""
+		},
+		"block": {
+			"style": "background: rgba(0,0,0,0); border: 0px; color: #ff000000; ",
+			"loc": [
+				1500,
+				202
+			],
+			"size": [
+				71,
+				22
+			],
+			"text": ""
+		},
+		"defaultmood": 18,
+		"moodlabel": {
+			"style": "",
+			"loc": [
+				0,
+				0
+			],
+			"text": ""
+		},
+		"moods": [
+			{
+				"style": "border: 0px; ",
+				"selected": "background-image: url($path/cherry.png); border: 0px; ",
+				"loc": [
+					210,
+					141
+				],
+				"size": [
+					217,
+					270
+				],
+				"text": "",
+				"icon": "$path/",
+				"mood": 18
+			},
+			{
+				"style": "border: 0px; color: #ff000000; ",
+				"selected": "border: 0px; color: #ff000000; ",
+				"loc": [
+					12,
+					117
+				],
+				"size": [
+					435,
+					18
+				],
+				"text": "",
+				"icon": "$path/",
+				"mood": 2
+			}
+		]
+	},
+	"convo": {
+		"style": "background: rgb(190, 19, 4); font-family: 'Arial'; ",
+		"scrollbar": {
+			"style": "",
+			"handle": ""
+		},
+		"margins": {
+			"top": 22,
+			"bottom": 9,
+			"left": 10,
+			"right": 4
+		},
+		"size": [
+			400,
+			250
+		],
+		"chumlabel": {
+			"style": "background-image: url($path/chumlabelbg.png); background-color: #ff000000; background-repeat: no-repeat; color: #ffffffff; padding: 2px; border: 1px solid #c2c2c2; margin-bottom: 4px; ",
+			"align": {
+				"h": "left",
+				"v": "center"
+			},
+			"minheight": 22,
+			"maxheight": 36,
+			"text": "cheering: $handle!"
+		},
+		"textarea": {
+			"style": "background: rgb(200, 200, 200); border: 2px solid #c2c2c2; font-size: 14px; "
+		},
+		"input": {
+			"style": "background: rgb(200, 200, 200); margin-top: 5px; border: 1px solid #c2c2c2; margin-right: 54px; font-size: 12px; "
+		},
+		"tabwindow": {
+			"style": "background: rgb(0, 118, 0); font-family: 'Arial'; "
+		},
+		"tabs": {
+			"style": "",
+			"selectedstyle": "",
+			"newmsgcolor": "#ff00ff11",
+			"tabstyle": 0
+		},
+		"text": {
+			"beganpester": "began cheering",
+			"ceasepester": "gave up cheering",
+			"blocked": "blocked",
+			"unblocked": "forgave",
+			"openmemo": "opened memo on board",
+			"joinmemo": "responded to memo",
+			"closememo": "ceased responding to memo",
+			"kickedmemo": "you're not allowed to speak..."
+		},
+		"systemMsgColor": "#ff646464"
+	},
+	"memos": {
+		"memoicon": "$path/memo.png",
+		"style": "background: rgb(0, 118, 0); font-family: 'Arial'; ",
+		"size": [
+			450,
+			300
+		],
+		"tabs": {
+			"style": "",
+			"selectedstyle": "",
+			"newmsgcolor": "#ff00ff1c",
+			"tabstyle": 0
+		},
+		"label": {
+			"text": "bulletin board?: $channel",
+			"style": "background-image: url($path/chumlabelbg.png); background-color: #ff000000; background-repeat: no-repeat; color: #ffffffff; padding: 2px; border: 1px solid #c2c2c2; margin-bottom: 4px; ",
+			"align": {
+				"h": "left",
+				"v": "center"
+			},
+			"minheight": 18,
+			"maxheight": 36
+		},
+		"textarea": {
+			"style": "background: rgb(200, 200, 200); border: 2px solid #c2c2c2; font-size: 12px; "
+		},
+		"input": {
+			"style": "background: rgb(200, 200, 200); margin-top: 5px; border: 1px solid #c2c2c2; font-size: 12px; margin-bottom: 5px; "
+		},
+		"margins": {
+			"top": 22,
+			"bottom": 10,
+			"left": 9,
+			"right": 4
+		},
+		"userlist": {
+			"width": 125,
+			"style": "font-size: 12px; background: rgb(200, 200, 200); margin-bottom: 5px; border: 2px solid #c2c2c2; padding: 5px; font-family: 'Arial'; selection-background-color: #ff000000; "
+		},
+		"time": {
+			"text": {
+				"width": 75,
+				"style": "color: #ff000000; font: bold; border: 1px solid #c2c2c2; background: rgb(200, 200, 200); height: 19px; "
+			},
+			"slider": {
+				"style": "border: 1px solid #00c2c2c2; ",
+				"groove": "border-image: url($path/timeslider.png); ",
+				"handle": "image: url($path/chummy.png); "
+			},
+			"buttons": {
+				"style": "border: 1px solid #00ffffff; height: 17px; width: 50px; color: #ff007600; font-family: 'Arial'; background: rgb(190, 19, 4); margin-left: 2px; "
+			},
+			"arrows": {
+				"left": "$path/leftarrow.png",
+				"right": "$path/rightarrow.png",
+				"style": "width: 19px; height: 19px; border: 0px; margin-left: 2px; "
+			}
+		},
+		"systemMsgColor": "#ff646464",
+		"op": {
+			"icon": "$path/op.png"
+		},
+		"halfop": {
+			"icon": "$path/halfop.png"
+		},
+		"voice": {
+			"icon": "$path/voice.png"
+		},
+		"founder": {
+			"icon": "$path/founder.png"
+		}
+	},
+	"toasts": {
+		"width": 210,
+		"height": 100,
+		"style": "background: rgb(0, 118, 0); ",
+		"icon": {
+			"signin": "$path/trayicon.png",
+			"signout": "$path/trayicon2.png",
+			"style": "border: 0px; "
+		},
+		"title": {
+			"minimumheight": 50,
+			"style": "border: 0px; padding: 5px; font-weight: bold; color: #ffffffff; "
+		},
+		"content": {
+			"style": "background: rgb(177, 177, 177); color: #ff000000; padding: 5px; "
+		}
+	}
+}
