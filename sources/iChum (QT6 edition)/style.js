4b543e0ff72b8597b894215ddda4a7253bd53f5d
anne <mocchapi@gmail.com>
2023-11-03 22:17:15 +0100

    Add iChum (QT6 version) by matytoonist

---
+++ sources/iChum (QT6 edition)/style.js
@@ -0,0 +1,65 @@
+{
+	"inherits": "iChum",
+	"main": {
+		"menus": {
+			"client": {
+				"_name": "   ",
+				"options": "Options",
+				"memos": "Public Chatrooms",
+				"logviewer": "Message Logs",
+				"randen": "Random Messaging",
+				"userlist": "Users Online",
+				"console": "Console",
+				"talk": "Message",
+				"addgroup": "Add Group",
+				"import": "Import",
+				"reconnect": "Reconnect",
+				"idle": "Idle",
+				"exit": "Quit"
+			},
+			"profile": {
+				"_name": "  ",
+				"switch": "Change user",
+				"color": "Color",
+				"theme": "Appearance",
+				"block": "Block List",
+				"quirks": "Quirks"
+			},
+			"help": {
+				"_name": "  ",
+				"about": "About",
+				"help": "Help",
+				"calsprite": "Lil' Cal",
+				"rules": "Rules",
+				"reportbug": "Report Error",
+				"chanserv": "Chatroom Moderation",
+				"nickserv": "Username Support"
+			},
+			"rclickchumlist": {
+				"pester": "Message",
+				"removechum": "Remove from contacts",
+				"report": "Report User",
+				"blockchum": "Block",
+				"addchum": "Add Contact",
+				"viewlog": "View Message Logs",
+				"notes": "Edit Notes",
+				"unblockchum": "Unblock",
+				"removegroup": "Remove Group",
+				"renamegroup": "Rename Group",
+				"movechum": "Move contact",
+				"banuser": "Ban User",
+				"opuser": "Give Operator",
+				"voiceuser": "Give Voice",
+				"quirkkill": "Kill Quirk",
+				"quirksoff": "Quirks Off",
+				"ooc": "OOC",
+				"invitechum": "Invite",
+				"memosetting": "Chatroom Settings",
+				"memonoquirk": "Disable Quirks",
+				"memohidden": "Hidden Chatroom",
+				"memoinvite": "Invite Only Chatroom",
+				"memomute": "Mute Chatroom"
+			}
+		}
+	}
+}
