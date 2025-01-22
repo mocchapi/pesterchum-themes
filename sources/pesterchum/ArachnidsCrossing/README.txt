310bd5fd89077d2448bdf0871bafb684973c46e8
mocha <mocchapi@gmail.com>
2025-01-22 04:04:06 +0100

    add ArachnidsCrossing by (lis)anne

---
+++ sources/pesterchum/ArachnidsCrossing/README.txt
@@ -0,0 +1,31 @@
+                          _           _     _      _____                   _             
+     /\                  | |         (_)   | |    / ____|                 (_)            
+    /  \   _ __ __ _  ___| |__  _ __  _  __| |___| |     _ __ ___  ___ ___ _ _ __   __ _ 
+   / /\ \ | '__/ _` |/ __| '_ \| '_ \| |/ _` / __| |    | '__/ _ \/ __/ __| | '_ \ / _` |
+  / ____ \| | | (_| | (__| | | | | | | | (_| \__ \ |____| | | (_) \__ \__ \ | | | | (_| |
+ /_/    \_\_|  \__,_|\___|_| |_|_| |_|_|\__,_|___/\_____|_|  \___/|___/___/_|_| |_|\__, |
+                                                                                    __/ |
+                                                                                   |___/ 
+   A theme based on Animal Crossing: Wild World (nds)
+    by (lis)anne / mocha                january 2025
+
+
+
+## Made with pesterchum theme editor 2000 :)
+https://mocchapi.itch.io/pesterchum-theme-editor-2000
+
+## attribution:
+- The main background & item_ring were taken directly from the game with DeSmuMe
+- The icon was taken from The Spriters Resource, submitted by Alxala:
+	- https://www.spriters-resource.com/ds_dsi/animalxingwildworld/sheet/225698/
+- The mood/expression icons were taken from Nookipedia.com:
+	- https://nookipedia.com/wiki/Reaction#In_Wild_World
+
+- The open/close pm sound was taken directly from the game with DeSmuMe
+- The namealert & memo sound were taken from The sounds resource, submitted by Previous:
+	- https://www.sounds-resource.com/ds_dsi/acww/sound/1733/
+	- (memo is `488.wav`, namealert is `406.wav`)
+
+- The font is by "David" on DaFont, who seems to have created it from spritesheets
+	- https://www.dafont.com/animal-crossing-wild-world.font
+	- https://www.dafont.com/david-fens.d5063
---
+++ sources/pesterchum/ArachnidsCrossing/style.js
@@ -0,0 +1,502 @@
+{
+	"convo": {
+		"textarea": {
+			"style": "font-family: 'Animal Crossing: Wild World'; background-image: url($path/final assets/chat_bg.png); border-color: #ffdedede; border-width: 3px 3px 3px 3px; border-style: solid; margin-left: 10px; margin-right: 10px; "
+		},
+		"chumlabel": {
+			"align": {
+				"v": "center",
+				"h": "left"
+			},
+			"style": "background-color: #ffc6f7ff; border-width: 0px; margin-bottom: 5px; ",
+			"text": "Mailing $handle"
+		},
+		"input": {
+			"style": "font-family: 'Animal Crossing: Wild World'; background-color: #ffffffff; border-color: #ffd3d3d3; border-width: 2px 2px 2px 2px; border-style: solid; margin-left: 10px; margin-right: 10px; margin-top: 5px; "
+		},
+		"scrollbar": {
+			"uarrowstyle": "image: url($path/final assets/empty.png); ",
+			"darrowstyle": "image: url($path/final assets/empty.png); ",
+			"handle": "background-image: url($path/og resolution assets/scrollbar_handle.png); background-position: center; background-repeat: no-repeat; max-height: 10px; border-radius: 0px; border-style: solid; ",
+			"style": "border-width: 0xp; width: 16px; background: url($path/final assets/scrollbar_segment.png); "
+		},
+		"tabs": {
+			"selectedstyle": "",
+			"style": "",
+			"newmsgcolor": "#ff000000"
+		},
+		"tabwindow": {
+			"style": "font-family: 'Animal Crossing: Wild World'; background-color: #ffff9400; font-size: 20px; color: #ff000000; border-color: #ffffff94; border-radius: 8px; border-width: 3px 3px 3px 3px; border-style: solid; "
+		},
+		"style": "font-family: 'Animal Crossing: Wild World'; background-color: #ffff9400; font-size: 20px; color: #ff000000; ",
+		"size": [
+			800,
+			600
+		]
+	},
+	"memos": {
+		"tabs": {
+			"selectedstyle": "",
+			"style": ""
+		},
+		"time": {
+			"buttons": {
+				"style": "background-color: #ffc6f7ff; border-color: #ffc6f7ff; margin-left: 7px; "
+			},
+			"text": {
+				"style": "font-family: 'Animal Crossing: Wild World'; background-color: #ffc6f7ff; border-color: #ffc6f7ff; "
+			}
+		},
+		"userlist": {
+			"width": 175,
+			"style": "background-color: #ffff4a94; border-width: 2px 0px 2px 2px; border-top-right-radius: 0px; border-top-left-radius: 5px; border-bottom-left-radius: 5px; border-bottom-right-radius: 0px; border-style: solid; border-color: #ff9c0042; font-family: 'Animal Crossing: Wild World'; "
+		},
+		"label": {
+			"text": "Mailing $channel",
+			"align": {
+				"v": "center",
+				"h": "left"
+			},
+			"style": "background-color: #ffc6f7ff; border-width: 0px; margin-bottom: 5px; "
+		},
+		"tabwindow": {
+			"style": "font-family: 'Animal Crossing: Wild World'; font-size: 16px; background-color: #ffff9400; font-size: 20px; color: #ff000000; "
+		},
+		"style": "font-family: 'Animal Crossing: Wild World'; background-color: #ffff9400; font-size: 20px; color: #ff000000; ",
+		"size": [
+			800,
+			600
+		],
+		"scrollbar": {
+			"uarrowstyle": "image: url($path/final assets/empty.png); ",
+			"darrowstyle": "image: url($path/final assets/empty.png); ",
+			"handle": "background-image: url($path/og resolution assets/scrollbar_handle.png); background-position: center; background-repeat: no-repeat; max-height: 10px; border-radius: 0px; border-style: solid; ",
+			"style": "border-width: 0xp; width: 16px; background: url($path/final assets/scrollbar_segment.png); "
+		},
+		"input": {
+			"style": "font-family: 'Animal Crossing: Wild World'; background-color: #ffffffff; border-color: #ffd3d3d3; border-width: 2px 2px 2px 2px; border-style: solid; margin-left: 10px; margin-right: 10px; margin-top: 5px; "
+		},
+		"textarea": {
+			"style": "font-family: 'Animal Crossing: Wild World'; background-image: url($path/final assets/chat_bg.png); border-color: #ffdedede; border-width: 3px 3px 3px 3px; border-style: solid; margin-left: 10px; margin-right: 10px; "
+		},
+		"memoicon": "$path/og resolution assets/papers.png"
+	},
+	"metadata": {
+		"license": "GPLv3",
+		"source": "Null",
+		"author": "(lis)anne"
+	},
+	"main": {
+		"newmsgicon": "$path/og resolution assets/icon_notif.png",
+		"windowtitle": "ArachnidsCrossing",
+		"block": {
+			"size": [
+				0,
+				0
+			],
+			"loc": [
+				0,
+				-60
+			]
+		},
+		"addchum": {
+			"size": [
+				0,
+				0
+			],
+			"loc": [
+				0,
+				-60
+			]
+		},
+		"trollslum": {
+			"size": [
+				128,
+				384
+			],
+			"style": "font-family: 'Animal Crossing: Wild World'; background-color: #ffff9400; color: #ff000000; "
+		},
+		"pester": {
+			"size": [
+				0,
+				0
+			],
+			"loc": [
+				0,
+				-60
+			]
+		},
+		"style": "font-family: 'Animal Crossing: Wild World'; background-color: #ffff9400; border-color: #ffffff94; border-style: solid; border-width: 2px 2px 2px 2px; ",
+		"moodlabel": {
+			"text": "",
+			"loc": [
+				0,
+				-50
+			]
+		},
+		"defaultmood": 0,
+		"minimize": {
+			"image": "$path/final assets/empty.png",
+			"loc": [
+				485,
+				35
+			]
+		},
+		"close": {
+			"image": "$path/final assets/empty.png",
+			"loc": [
+				473,
+				6
+			]
+		},
+		"menus": {
+			"help": {
+				"_name": "     "
+			},
+			"profile": {
+				"_name": "     "
+			},
+			"client": {
+				"_name": "     "
+			}
+		},
+		"defaultwindow": {
+			"style": "font-family: 'Animal Crossing: Wild World'; background-color: #ffff9400; font-size: 20px; color: #ff000000; "
+		},
+		"moods": [
+			{
+				"icon": "$path/final assets/large moods/chummy_Laughter_WW_Icon_2x.png",
+				"text": "",
+				"mood": 0,
+				"style": "background-image: url($path/final assets/item_ring_2x.png); ",
+				"selected": "background-image: url($path/final assets/item_ring_selected_2x.png); ",
+				"loc": [
+					15,
+					223
+				],
+				"size": [
+					49,
+					49
+				]
+			},
+			{
+				"icon": "$path/final assets/large moods/rancorus_Rage_WW_Icon_2x.png",
+				"text": "",
+				"mood": 1,
+				"style": "background-image: url($path/final assets/item_ring_2x.png); ",
+				"selected": "background-image: url($path/final assets/item_ring_selected_2x.png); ",
+				"loc": [
+					79,
+					223
+				],
+				"size": [
+					49,
+					49
+				]
+			},
+			{
+				"icon": "$path/final assets/large moods/distraught_Distress_WW_Icon_2x.png",
+				"text": "",
+				"mood": 4,
+				"style": "background-image: url($path/final assets/item_ring_2x.png); ",
+				"selected": "background-image: url($path/final assets/item_ring_selected_2x.png); ",
+				"loc": [
+					143,
+					223
+				],
+				"size": [
+					49,
+					49
+				]
+			},
+			{
+				"icon": "$path/final assets/large moods/pranky_Scheming_WW_Icon_2x.png",
+				"text": "",
+				"mood": 5,
+				"style": "background-image: url($path/final assets/item_ring_2x.png); ",
+				"selected": "background-image: url($path/final assets/item_ring_selected_2x.png); ",
+				"loc": [
+					207,
+					223
+				],
+				"size": [
+					49,
+					49
+				]
+			},
+			{
+				"icon": "$path/final assets/large moods/smooth_Acceptance_WW_Icon_2x.png",
+				"text": "",
+				"mood": 6,
+				"style": "background-image: url($path/final assets/item_ring_2x.png); ",
+				"selected": "background-image: url($path/final assets/item_ring_selected_2x.png); ",
+				"loc": [
+					271,
+					223
+				],
+				"size": [
+					49,
+					49
+				]
+			},
+			{
+				"icon": "$path/final assets/large moods/ecstatic_Happiness_WW_Icon_2x.png",
+				"text": "",
+				"mood": 7,
+				"style": "background-image: url($path/final assets/item_ring_2x.png); ",
+				"selected": "background-image: url($path/final assets/item_ring_selected_2x.png); ",
+				"loc": [
+					47,
+					271
+				],
+				"size": [
+					49,
+					49
+				]
+			},
+			{
+				"icon": "$path/final assets/large moods/relaxed_Agreement_WW_Icon_2x.png",
+				"text": "",
+				"mood": 8,
+				"style": "background-image: url($path/final assets/item_ring_2x.png); ",
+				"selected": "background-image: url($path/final assets/item_ring_selected_2x.png); ",
+				"loc": [
+					111,
+					271
+				],
+				"size": [
+					49,
+					49
+				]
+			},
+			{
+				"icon": "$path/final assets/large moods/discontent_Discontent_WW_Icon_2x.png",
+				"text": "",
+				"mood": 9,
+				"style": "background-image: url($path/final assets/item_ring_2x.png); ",
+				"selected": "background-image: url($path/final assets/item_ring_selected_2x.png); ",
+				"loc": [
+					175,
+					271
+				],
+				"size": [
+					49,
+					49
+				]
+			},
+			{
+				"icon": "$path/final assets/large moods/detestful_Irritation_WW_Icon_2x.png",
+				"text": "",
+				"mood": 12,
+				"style": "background-image: url($path/final assets/item_ring_2x.png); ",
+				"selected": "background-image: url($path/final assets/item_ring_selected_2x.png); ",
+				"loc": [
+					239,
+					271
+				],
+				"size": [
+					49,
+					49
+				]
+			},
+			{
+				"icon": "$path/final assets/large moods/vigorous_Sorrow_WW_Icon_2x.png",
+				"text": "",
+				"mood": 15,
+				"style": "background-image: url($path/final assets/item_ring_2x.png); ",
+				"selected": "background-image: url($path/final assets/item_ring_selected_2x.png); ",
+				"loc": [
+					303,
+					271
+				],
+				"size": [
+					49,
+					49
+				]
+			},
+			{
+				"icon": "$path/final assets/large moods/acceptant_Disbelief_WW_Icon_2x.png",
+				"text": "",
+				"mood": 17,
+				"style": "background-image: url($path/final assets/item_ring_2x.png); ",
+				"selected": "background-image: url($path/final assets/item_ring_selected_2x.png); ",
+				"loc": [
+					79,
+					319
+				],
+				"size": [
+					49,
+					49
+				]
+			},
+			{
+				"icon": "$path/final assets/large moods/amazed_Realization_WW_Icon_2x.png",
+				"text": "",
+				"mood": 20,
+				"style": "background-image: url($path/final assets/item_ring_2x.png); ",
+				"selected": "background-image: url($path/final assets/item_ring_selected_2x.png); ",
+				"loc": [
+					143,
+					319
+				],
+				"size": [
+					49,
+					49
+				]
+			}
+		],
+		"fonts": [
+			"$path/fonts/Animal-Crossing-Wild-World.ttf"
+		],
+		"mychumhandle": {
+			"currentMood": [
+				0,
+				-60
+			],
+			"colorswatch": {
+				"style": "color: #00000000; display: none; background-color: #00000000; ",
+				"size": [
+					43,
+					26
+				],
+				"loc": [
+					155,
+					5
+				]
+			},
+			"label": {
+				"text": "",
+				"loc": [
+					0,
+					-60
+				]
+			},
+			"handle": {
+				"style": "background-color: #ff000000; border-width: 0px; border-color: #ffffffff; color: #ff000000; font-family: 'Animal Crossing: Wild World'; font-size: 18px; ",
+				"size": [
+					135,
+					26
+				],
+				"loc": [
+					65,
+					100
+				]
+			}
+		},
+		"sounds": {
+			"namealarm": "$path/sounds/406 - shocked.wav",
+			"memosound": "$path/sounds/488 -  notification.wav",
+			"alertsound": "$path/sounds/pesterwindow_open_close.wav",
+			"ceasesound": "$path/sounds/pesterwindow_open_close.wav"
+		},
+		"menu": {
+			"selected": "color: #ff000000; background-color: #ffffff94; ",
+			"style": "border-color: #ffffff94; border-width: 2px 2px 2px 2px; background-color: #ffff9400; border-style: solid; border-radius: 8px; font-family: 'Animal Crossing: Wild World'; font-size: 18px; ",
+			"loc": [
+				197,
+				10
+			]
+		},
+		"chums": {
+			"scrollbar": {
+				"uarrowstyle": "image: url($path/final assets/empty.png); ",
+				"darrowstyle": "image: url($path/final assets/empty.png); ",
+				"handle": "background-image: url($path/og resolution assets/scrollbar_handle.png); background-position: center; background-repeat: no-repeat; max-height: 10px; border-radius: 0px; border-style: solid; ",
+				"style": "border-width: 0xp; min-height: 18px; background: #ffff4a94; ",
+				"uparrow": "",
+				"downarrow": ""
+			},
+			"moods": {
+				"blocked": {
+					"icon": "$path/og resolution assets/expressions/Blankness_WW_Icon.png"
+				},
+				"bemused": {
+					"icon": "$path/og resolution assets/expressions/Thought_WW_Icon.png"
+				},
+				"insolent": {
+					"icon": "$path/og resolution assets/expressions/Inspiration_WW_Icon.png"
+				},
+				"amazed": {
+					"icon": "$path/og resolution assets/expressions/Realization_WW_Icon.png"
+				},
+				"mystified": {
+					"icon": "$path/og resolution assets/expressions/Confusion_WW_Icon.png"
+				},
+				"protective": {
+					"icon": "$path/og resolution assets/expressions/Joy_WW_Icon.png"
+				},
+				"acceptant": {
+					"icon": "$path/og resolution assets/expressions/Disbelief_WW_Icon.png"
+				},
+				"perky": {
+					"icon": "$path/og resolution assets/expressions/Agreement_WW_Icon.png"
+				},
+				"vigorous": {
+					"icon": "$path/og resolution assets/expressions/Sorrow_WW_Icon.png"
+				},
+				"manipulative": {
+					"icon": "$path/og resolution assets/expressions/Laughter_WW_Icon.png"
+				},
+				"mirthful": {
+					"icon": "$path/og resolution assets/expressions/Approval_WW_Icon.png"
+				},
+				"detestful": {
+					"icon": "$path/og resolution assets/expressions/Irritation_WW_Icon.png"
+				},
+				"sleek": {
+					"icon": "$path/og resolution assets/expressions/Acceptance_WW_Icon.png"
+				},
+				"devious": {
+					"icon": "$path/og resolution assets/expressions/Scheming_WW_Icon.png"
+				},
+				"discontent": {
+					"icon": "$path/og resolution assets/expressions/Discontent_WW_Icon.png"
+				},
+				"relaxed": {
+					"icon": "$path/og resolution assets/expressions/Agreement_WW_Icon.png"
+				},
+				"ecstatic": {
+					"icon": "$path/og resolution assets/expressions/Happiness_WW_Icon.png"
+				},
+				"smooth": {
+					"icon": "$path/og resolution assets/expressions/Acceptance_WW_Icon.png"
+				},
+				"pranky": {
+					"icon": "$path/og resolution assets/expressions/Scheming_WW_Icon.png"
+				},
+				"distraught": {
+					"icon": "$path/og resolution assets/expressions/Distress_WW_Icon.png"
+				},
+				"pleasant": {
+					"icon": "$path/og resolution assets/expressions/Contentment_WW_Icon.png"
+				},
+				"offline": {
+					"icon": "$path/og resolution assets/expressions/Fatigue_WW_Icon.png"
+				},
+				"rancorous": {
+					"icon": "$path/og resolution assets/expressions/Rage_WW_Icon.png"
+				},
+				"chummy": {
+					"icon": "$path/og resolution assets/expressions/Laughter_WW_Icon.png"
+				}
+			},
+			"style": "font-size: 15px; background-color: #ffff4a94; border-width: 4px; border-color: #ff9c0042; font-family: 'Animal Crossing: Wild World'; ",
+			"userlistcolor": "#ff000000",
+			"size": [
+				84,
+				228
+			],
+			"loc": [
+				406,
+				119
+			]
+		},
+		"size": [
+			512,
+			384
+		],
+		"icon": "$path/final assets/icon.png",
+		"background-image": "$path/final assets/bg_2x.png"
+	},
+	"inherits": "pesterchum"
+}
